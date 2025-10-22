b4d26d5f1877be5275ca8c43b404330b6aafd0df
Madhav <madhavadas@google.com>
2025-10-22 10:49:27 -0700

    Add SELinux policy for usbauthservice
    This change introduces a new SELinux domain and associated policies for the `usbauthservice` daemon. It defines types for the service, its executable, and grants necessary permissions to interact with USB-related sysfs files, specifically within `/sys/module/usbcore` and `/sys/bus/usb/devices`. The policies are versioned to be effective starting from board API level 202604.
    
    Bug: 432527670
    Flag: build.RELEASE_DESKTOP_USB_AUTH_SERVICE_ENABLED
    Test: build and deploy on brya device
    Change-Id: I17b91a4e98eaef1b0bb5f0609c8e902b915b0486

--- build/soong/service_fuzzer_bindings.go
+++ build/soong/service_fuzzer_bindings.go
@@ -523,6 +523,7 @@ var (
 		"uri_grants":                             EXCEPTION_NO_FUZZER,
 		"usagestats":                             EXCEPTION_NO_FUZZER,
 		"usb":                                    EXCEPTION_NO_FUZZER,
+		"usb_auth":                               EXCEPTION_NO_FUZZER,
 		"uprobestats_service":                    EXCEPTION_NO_FUZZER,
 		"uprobestats_bridge_service":             EXCEPTION_NO_FUZZER,
 		"user":                                   EXCEPTION_NO_FUZZER,
