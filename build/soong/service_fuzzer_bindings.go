ab9ed3093bee1fc4c29e96334369eb3c80805078
Pratik Jain <ppjain@google.com>
2026-02-16 08:17:39 +0000

    sepolicy: Allow KeyMint HAL to access IProvisioning
    Grant the KeyMint HAL for system VMs the necessary SELinux permissions
    to find and communicate with the IProvisioning AIDL service
    
    Bug: 478166729
    Test: atest VtsHalRemotelyProvisionedComponentTargettest
    Change-Id: I9493866d4cbbe4a26ac23525d5b9b90bd027e8aa

--- build/soong/service_fuzzer_bindings.go
+++ build/soong/service_fuzzer_bindings.go
@@ -222,6 +222,7 @@ var (
 		"android.os.IAccessor/IKeyMintProvisioningService/security_vm_keymint": EXCEPTION_NO_FUZZER,
 		"com.android.trusty.rust.KeyMintProvisioningService.V1":                EXCEPTION_NO_FUZZER,
 		"android.os.IAccessor/IProvisioning/security_vm_keymint":               EXCEPTION_NO_FUZZER,
+		"android.trusty.provisioning.IProvisioning/security_vm_keymint":        EXCEPTION_NO_FUZZER,
 		"android.system.virtualizationservice_internal.IVfioHandler":           EXCEPTION_NO_FUZZER,
 		"android.system.virtualizationservice_internal.IVmnic":                 EXCEPTION_NO_FUZZER,
 		"android.system.virtualizationmaintenance":                             EXCEPTION_NO_FUZZER,
